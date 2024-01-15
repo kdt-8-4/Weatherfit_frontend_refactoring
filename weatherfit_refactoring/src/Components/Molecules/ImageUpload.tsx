07a44d7cfeb785fbd637172f948fcd6c2bcd374c
hyeiis <hyeis0807@gmail.com>
2024-01-15 22:36:12 +0900

    [Feat] #3 이미지 업로드 molecule

---
+++ weatherfit_refactoring/src/Components/Molecules/ImageUpload.tsx
@@ -0,0 +1,5 @@
+import InputStore, { InputStyle } from "@/Components/Atoms/Input/InputStore";
+
+export default function ImageUpload () {
+    return (<><InputStore inputStyle={InputStyle.INPUT_IMAGE} /></>);
+}

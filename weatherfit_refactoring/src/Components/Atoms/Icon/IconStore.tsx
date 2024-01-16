08bb2b04148e7f261835ca84d9897b06b63d0d0b
hyeiis <hyeis0807@gmail.com>
2024-01-16 12:20:02 +0900

    [Feat] #3 Prettier 오류 수정

--- weatherfit_refactoring/src/Components/Atoms/Icon/IconStore.tsx
+++ weatherfit_refactoring/src/Components/Atoms/Icon/IconStore.tsx
@@ -1,200 +1,230 @@
-import Image from "next/image";
+import Image from 'next/image'
 
 export enum IconStyle {
-    MY_PROFILE = "MY_PROFILE",
-    MY_PROFILE_FILL = "MY_PROFILE_FILL",
-    UPLOAD_FILL = "UPLOAD_FILL",
-    UPLOAD = "UPLOAD",
-    HOME_FILL = "HOME_FILL",
-    HOME = "HOME",
-    COMMENTS = "COMMENTS",
-    UNLIKE = "UNLIKE",
-    LIKE = "LIKE",
-    TOGGLE = "TOGGLE",
-    TOGGLE2 = "TOGGLE2",
-    PREV = "PREV",
-    PREV2 = "PREV2",
-    NEXT = "NEXT",
-    SETTING = "SETTING",
-    ETC = "ETC",
-    LOWEST = "LOWEST",
-    HIGHEST = "HIGHEST",
-    SEARCH = "SEARCH",
+  MY_PROFILE = 'MY_PROFILE',
+  MY_PROFILE_FILL = 'MY_PROFILE_FILL',
+  UPLOAD_FILL = 'UPLOAD_FILL',
+  UPLOAD = 'UPLOAD',
+  HOME_FILL = 'HOME_FILL',
+  HOME = 'HOME',
+  COMMENTS = 'COMMENTS',
+  UNLIKE = 'UNLIKE',
+  LIKE = 'LIKE',
+  TOGGLE = 'TOGGLE',
+  TOGGLE2 = 'TOGGLE2',
+  PREV = 'PREV',
+  PREV2 = 'PREV2',
+  NEXT = 'NEXT',
+  SETTING = 'SETTING',
+  ETC = 'ETC',
+  LOWEST = 'LOWEST',
+  HIGHEST = 'HIGHEST',
+  SEARCH = 'SEARCH',
 }
 
 interface Props {
-    iconStyle: IconStyle;
-    size?:number;
-    style?:string;
+  iconStyle: IconStyle
+  size?: number
+  style?: string
 }
 
-
-
-export default function IconStore({iconStyle, size, style}:Props) {
-    const selectIcon = (): React.ReactNode => {
-        switch (iconStyle) {
-            case IconStyle.MY_PROFILE_FILL:
-                return <Image 
-                src={"/icon_svg/my_profile_fill.svg"} 
-                alt="my_profile_fill"
-                width={size}
-                height={size}
-                className={`${style}`}
-                />
-            case IconStyle.MY_PROFILE:
-                return <Image
-                src={"/icon_svg/my_profile.svg"} 
-                alt="my_profile"
-                width={size}
-                height={size}
-                className={`${style}`}
-                />
-            case IconStyle.UPLOAD_FILL:
-                return <Image 
-                src={"/icon_svg/upload_fill.svg"} 
-                alt="upload_fill"
-                width={size}
-                height={size}
-                className={`${style}`}
-                />
-            case IconStyle.UPLOAD:
-                return <Image 
-                src={"/icon_svg/upload.svg"} 
-                alt="upload"
-                width={size}
-                height={size}
-                className={`${style}`}
-                />
-            case IconStyle.HOME_FILL:
-                return <Image 
-                src={"/icon_svg/home_fill.svg"} 
-                alt="home_fill"
-                width={size}
-                height={size}
-                className={`${style}`}
-                />
-            case IconStyle.HOME:
-                return <Image 
-                src={"/icon_svg/home.svg"} 
-                alt="home"
-                width={size}
-                height={size}
-                className={`${style}`}
-                />
-            case IconStyle.COMMENTS:
-                return <Image 
-                src={"/icon_svg/comments.svg"} 
-                alt="comments"
-                width={size}
-                height={size}
-                className={`${style}`}
-                />
-            case IconStyle.UNLIKE:
-                return <Image 
-                src={"/icon_svg/unlike.svg"} 
-                alt="unlike"
-                width={size}
-                height={size}
-                className={`${style}`}
-                />
-            case IconStyle.LIKE:
-                return <Image 
-                src={"/icon_svg/like.svg"} 
-                alt="like"
-                width={size}
-                height={size}
-                className={`${style}`}
-                />
-            case IconStyle.TOGGLE:
-                return <Image 
-                src={"/icon_svg/toggle.svg"} 
-                alt="toggle"
-                width={size}
-                height={size}
-                className={`${style}`}
-                />
-            case IconStyle.TOGGLE2:
-                return <Image 
-                src={"/icon_svg/toggle2.svg"} 
-                alt="toggle2"
-                width={size}
-                height={size}
-                className={`${style}`}
-                />    
-            case IconStyle.PREV:
-                return <Image 
-                src={"/icon_svg/prev.svg"} 
-                alt="prev"
-                width={size}
-                height={size}
-                className={`${style}`}
-                />
-            case IconStyle.PREV2:
-                return <Image 
-                src={"/icon_svg/prev2.svg"} 
-                alt="prev2"
-                width={size}
-                height={size}
-                className={`${style}`}
-                />
-            case IconStyle.NEXT:
-                return <Image 
-                src={"/icon_svg/next.svg"} 
-                alt="next"
-                width={size}
-                height={size}
-                className={`${style}`}
-                />
-            case IconStyle.SETTING:
-                return <Image 
-                src={"/icon_svg/setting.svg"} 
-                alt="setting"
-                width={size}
-                height={size}
-                className={`${style}`}
-                />
-            case IconStyle.ETC:
-                return <Image 
-                src={"/icon_svg/etc.svg"} 
-                alt="etc"
-                width={size}
-                height={size}
-                className={`${style}`}
-                />
-            case IconStyle.HIGHEST:
-                return <Image 
-                src={"/icon_svg/highest.svg"} 
-                alt="highest"
-                width={size}
-                height={size}
-                className={`${style}`}
-                />
-            case IconStyle.LOWEST:
-                return <Image 
-                src={"/icon_svg/lowest.svg"} 
-                alt="lowest"
-                width={size}
-                height={size}
-                className={`${style}`}
-                />
-            case IconStyle.SEARCH:
-                return <Image 
-                src={"/icon_svg/search.svg"} 
-                alt="search"
-                width={size}
-                height={size}
-                className={`${style}`}
-                />
-            default:
-                return null;
-        }
+export default function IconStore({ iconStyle, size, style }: Props) {
+  const selectIcon = (): React.ReactNode => {
+    switch (iconStyle) {
+      case IconStyle.MY_PROFILE_FILL:
+        return (
+          <Image
+            src={'/icon_svg/my_profile_fill.svg'}
+            alt="my_profile_fill"
+            width={size}
+            height={size}
+            className={`${style}`}
+          />
+        )
+      case IconStyle.MY_PROFILE:
+        return (
+          <Image
+            src={'/icon_svg/my_profile.svg'}
+            alt="my_profile"
+            width={size}
+            height={size}
+            className={`${style}`}
+          />
+        )
+      case IconStyle.UPLOAD_FILL:
+        return (
+          <Image
+            src={'/icon_svg/upload_fill.svg'}
+            alt="upload_fill"
+            width={size}
+            height={size}
+            className={`${style}`}
+          />
+        )
+      case IconStyle.UPLOAD:
+        return (
+          <Image
+            src={'/icon_svg/upload.svg'}
+            alt="upload"
+            width={size}
+            height={size}
+            className={`${style}`}
+          />
+        )
+      case IconStyle.HOME_FILL:
+        return (
+          <Image
+            src={'/icon_svg/home_fill.svg'}
+            alt="home_fill"
+            width={size}
+            height={size}
+            className={`${style}`}
+          />
+        )
+      case IconStyle.HOME:
+        return (
+          <Image
+            src={'/icon_svg/home.svg'}
+            alt="home"
+            width={size}
+            height={size}
+            className={`${style}`}
+          />
+        )
+      case IconStyle.COMMENTS:
+        return (
+          <Image
+            src={'/icon_svg/comments.svg'}
+            alt="comments"
+            width={size}
+            height={size}
+            className={`${style}`}
+          />
+        )
+      case IconStyle.UNLIKE:
+        return (
+          <Image
+            src={'/icon_svg/unlike.svg'}
+            alt="unlike"
+            width={size}
+            height={size}
+            className={`${style}`}
+          />
+        )
+      case IconStyle.LIKE:
+        return (
+          <Image
+            src={'/icon_svg/like.svg'}
+            alt="like"
+            width={size}
+            height={size}
+            className={`${style}`}
+          />
+        )
+      case IconStyle.TOGGLE:
+        return (
+          <Image
+            src={'/icon_svg/toggle.svg'}
+            alt="toggle"
+            width={size}
+            height={size}
+            className={`${style}`}
+          />
+        )
+      case IconStyle.TOGGLE2:
+        return (
+          <Image
+            src={'/icon_svg/toggle2.svg'}
+            alt="toggle2"
+            width={size}
+            height={size}
+            className={`${style}`}
+          />
+        )
+      case IconStyle.PREV:
+        return (
+          <Image
+            src={'/icon_svg/prev.svg'}
+            alt="prev"
+            width={size}
+            height={size}
+            className={`${style}`}
+          />
+        )
+      case IconStyle.PREV2:
+        return (
+          <Image
+            src={'/icon_svg/prev2.svg'}
+            alt="prev2"
+            width={size}
+            height={size}
+            className={`${style}`}
+          />
+        )
+      case IconStyle.NEXT:
+        return (
+          <Image
+            src={'/icon_svg/next.svg'}
+            alt="next"
+            width={size}
+            height={size}
+            className={`${style}`}
+          />
+        )
+      case IconStyle.SETTING:
+        return (
+          <Image
+            src={'/icon_svg/setting.svg'}
+            alt="setting"
+            width={size}
+            height={size}
+            className={`${style}`}
+          />
+        )
+      case IconStyle.ETC:
+        return (
+          <Image
+            src={'/icon_svg/etc.svg'}
+            alt="etc"
+            width={size}
+            height={size}
+            className={`${style}`}
+          />
+        )
+      case IconStyle.HIGHEST:
+        return (
+          <Image
+            src={'/icon_svg/highest.svg'}
+            alt="highest"
+            width={size}
+            height={size}
+            className={`${style}`}
+          />
+        )
+      case IconStyle.LOWEST:
+        return (
+          <Image
+            src={'/icon_svg/lowest.svg'}
+            alt="lowest"
+            width={size}
+            height={size}
+            className={`${style}`}
+          />
+        )
+      case IconStyle.SEARCH:
+        return (
+          <Image
+            src={'/icon_svg/search.svg'}
+            alt="search"
+            width={size}
+            height={size}
+            className={`${style}`}
+          />
+        )
+      default:
+        return null
     }
+  }
 
-    return (
-        <>
-        {
-            selectIcon()
-        }
-        </>
-    )
+  return <>{selectIcon()}</>
 }
--- weatherfit_refactoring/src/Components/Atoms/Input/InputStore.tsx
+++ weatherfit_refactoring/src/Components/Atoms/Input/InputStore.tsx
@@ -1,48 +1,55 @@
-import Image from "next/image";
-import { FormEventHandler} from "react";
+import Image from 'next/image'
+import { FormEventHandler } from 'react'
 
 export enum InputStyle {
-    INPUT_WHITE = "INPUT_WHITE",
-    INPUT_IMAGE = "INPUT_IMAGE",
+  INPUT_WHITE = 'INPUT_WHITE',
+  INPUT_IMAGE = 'INPUT_IMAGE',
 }
 //INPUT_IMAGE는 크기 고정이라서 여기 아톰에서 w-40랑 h-64 수정해주면 돼요
 
 interface Props {
-    inputStyle: InputStyle;
-    placeholderContents?:string;
-    style?:string;
-    onChageFunction?: FormEventHandler<HTMLDivElement> | undefined;
+  inputStyle: InputStyle
+  placeholderContents?: string
+  style?: string
+  onChageFunction?: FormEventHandler<HTMLDivElement> | undefined
 }
 
-export default function InputStore({inputStyle,placeholderContents, onChageFunction, style}: Props) {
-    const selectInput = (): React.ReactNode => {
-        switch (inputStyle) {
-            case InputStyle.INPUT_WHITE:
-                return <input type="text" 
-                className={`border rounded-lg bg-white border-gray-500 p-1 ${style}`} 
-                placeholder={placeholderContents}
-                />
-            case InputStyle.INPUT_IMAGE:
-                return <div className="border rounded-2xl w-40 h-64 flex" style={{backgroundColor:"#F1EDED"}} onChange={onChageFunction}>
-                    <Image
-                    style={{margin:"auto", padding:"100px 0px" }}
-                    src={"/icon_svg/add.svg"}
-                    alt="add"
-                    width={25}
-                    height={25}
-                    />
-                    <input className="hidden" type="file" accept="image/*" multiple/>
-                </div>
-            default:
-                return null;
-        }
+export default function InputStore({
+  inputStyle,
+  placeholderContents,
+  onChageFunction,
+  style,
+}: Props) {
+  const selectInput = (): React.ReactNode => {
+    switch (inputStyle) {
+      case InputStyle.INPUT_WHITE:
+        return (
+          <input
+            type="text"
+            className={`border rounded-lg bg-white border-gray-500 p-1 ${style}`}
+            placeholder={placeholderContents}
+          />
+        )
+      case InputStyle.INPUT_IMAGE:
+        return (
+          <div
+            className="border rounded-2xl w-40 h-64 flex"
+            style={{ backgroundColor: '#F1EDED' }}
+            onChange={onChageFunction}>
+            <Image
+              style={{ margin: 'auto', padding: '100px 0px' }}
+              src={'/icon_svg/add.svg'}
+              alt="add"
+              width={25}
+              height={25}
+            />
+            <input className="hidden" type="file" accept="image/*" multiple />
+          </div>
+        )
+      default:
+        return null
     }
+  }
 
-    return (
-        <>
-        {
-            selectInput()
-        }
-        </>
-    )
+  return <>{selectInput()}</>
 }

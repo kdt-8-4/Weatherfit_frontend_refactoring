253bff830a8af107ae639069026fb8e2a788e9ba
hyeiis <hyeis0807@gmail.com>
2024-01-17 16:40:21 +0900

    [Feat] #3, #5  페이지 아토믹 수정

--- weatherfit_refactoring/src/Components/Molecules/UploadDate.tsx
+++ weatherfit_refactoring/src/Components/Molecules/UploadDate.tsx
@@ -1,5 +1,6 @@
 import ButtonStore, { ButtonStyle } from '../Atoms/Button/ButtonStore'
 
+// UploadWeather
 export default function UploadDate() {
   const date = new Date()
   const month = date.getMonth() + 1
@@ -7,12 +8,13 @@ export default function UploadDate() {
 
   return (
     <div className="flex mb-3 items-center w-fit">
-      <p className="font-gmarketsans pt-[5px] mr-3">업로드 날짜</p>
-      <ButtonStore
+      <p className="font-gmarketsans pt-[5px] mr-3">업로드 날씨</p>
+      {/* Weather API에서 날씨 아이콘 가져와서 보여주기 */}
+      {/* <ButtonStore
         buttonStyle={ButtonStyle.CONFIRM_BTN}
         style="font-NanumSquareRound rounded-full text-[12px] px-5 pt-[1px] cursor-default w-fit">
         {month}월 {day}일
-      </ButtonStore>
+      </ButtonStore> */}
     </div>
   )
 }

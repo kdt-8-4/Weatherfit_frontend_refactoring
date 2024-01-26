85f191aa08e3091457e846e22bbc45bf64f0bfdb
hyeiis <hyeis0807@gmail.com>
2024-01-26 13:48:01 +0900

    [Feat] #3 업로드 페이지 - 날씨 아이콘 불러오기

--- weatherfit_refactoring/src/Components/Molecules/UploadWeather.tsx
+++ weatherfit_refactoring/src/Components/Molecules/UploadWeather.tsx
@@ -1,8 +1,20 @@
+import { WeatherIcon } from '@/Store/WeatherIcon'
+import Image from 'next/image'
+
 export default function UploadWeather() {
+  const { weatherIcon } = WeatherIcon()
   return (
     <div className="flex mb-3 items-center w-fit">
       <p className="font-gmarketsans pt-[5px] mr-3 text-sm">업로드 날씨</p>
-      {/* Weather API에서 날씨 아이콘 가져와서 보여주기 */}
+      {weatherIcon && (
+        <Image
+          src={`https://openweathermap.org/img/wn/${weatherIcon}.png`}
+          alt="날씨"
+          width={10}
+          height={10}
+          loading="lazy"
+        />
+      )}
     </div>
   )
 }

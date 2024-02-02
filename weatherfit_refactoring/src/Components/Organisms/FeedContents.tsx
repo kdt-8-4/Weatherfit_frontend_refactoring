1f136f0335b12514a7eeb96e7d848a2748ea4c00
nebulaBdj <dongdung5070@gmail.com>
2024-02-02 20:45:18 +0900

    Feed #7 무한 캐러셀 구현 및 데이터 페칭

--- weatherfit_refactoring/src/Components/Organisms/FeedContents.tsx
+++ weatherfit_refactoring/src/Components/Organisms/FeedContents.tsx
@@ -1,3 +1,5 @@
+import axios from "axios";
+
 import ButtonStore, { ButtonStyle } from "../Atoms/Button/ButtonStore";
 import feedDummy from "../../../public/dummy_data/feed.json";
 import { FeedData } from "@/Store/FeedData";
@@ -7,66 +9,27 @@ import { WeatherTempMin } from "@/Store/WeatherMinTemp";
 import FeedContent from "../Molecules/FeedContent";
 import { useEffect } from "react";
 
-export default function FeedContents(){ 
+export default async function FeedContents (){ 
 
-    const { feedData, setFeedData } = FeedData();
-    const { feedSort } = FeedSort();
-    const {temperatureMax} = WeatherTempMax();
-    const {temperatureMin} = WeatherTempMin();
+    // const {feedData, setFeedData} = FeedData();
 
-    console.log("피드 더미 데이터", feedDummy.feed_data);
-    console.log("최고 최저", temperatureMax, temperatureMin);
+    const feedDataFetch = await fetch("https://www.jerneithe.site/board/list",{
+        method : "GET"
+    })
 
-    const sortedData: FEEDATA[] = [...feedDummy.feed_data].sort((a :FEEDATA, b: FEEDATA) => {
-        const dateA = new Date(a.createDate);
-        const dateB = new Date(b.createDate);
-        return dateB.getTime() - dateA.getTime(); // 최신 날짜 순으로 정렬
-    });
+    const feedContentsData:FEEDDATA[] = await feedDataFetch.json();
     
-    const filterFunction = () => {
-        const filterByTemperature = sortedData.filter(
-            (item) => 
-            item.temperature >= temperatureMin &&
-            item.temperature <= temperatureMax,
-        )
     
-        setFeedData(sortedData);
-    }
-
-    //useEffect를 안쓰면 무한루프 돌던데 어떻게 해야하나요..
-    useEffect(() => {
-        filterFunction();
-    },[]);
-
-    return (<div>
-        <div className="my-3 text-center">
-            <ButtonStore 
-                buttonStyle={ButtonStyle.DEFAULT_BTN_BLUE} 
-                style={`font-neurimboGothic font-semibold text-[15px] bg-A8C6EC m-auto pt-[0px] pb-[4px] w-[70px]`}>
-                {feedSort}
-            </ButtonStore>
-        </div>
-        <div className="mt-5 flex flex-wrap">
-            {feedData ? (
-                feedData.map((arr)=>{
-                    return(<FeedContent 
-                        images={arr.images} 
-                        nickName={arr.nickName} 
-                        temperature={arr.temperature} 
-                        weatherIcon={arr.weatherIcon}
-                        
-                        boardId={arr.boardId}
-                        createDate={arr.createDate}
-                        likeCount={arr.likeCount}
-                        likelist={arr.likelist}
-                        weather={arr.weather}
-                        />)
-                })
-            ) : (
-                <div className=" font-gmarketsans">데이터를 불러오고 있습니다.</div>
+    return (<div className="mt-5 flex flex-wrap">
+        {feedContentsData.map((feedDataArr)=>{
+            return(
+                <div key={feedDataArr.boardId}>
+                    <FeedContent 
+                        feedData={feedDataArr}
+                    />
+                </div>
             )
-            }
-            
-        </div>
+
+        })}
     </div>)
 }

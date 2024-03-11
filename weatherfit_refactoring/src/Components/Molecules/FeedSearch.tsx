5b2da980c19166aa66c76403153699b2faa94c98
nebulaBdj <dongdung5070@gmail.com>
2024-03-11 10:42:49 +0900

    [Feat]해시태그 검색 기능 완료 Feed #7

--- weatherfit_refactoring/src/Components/Molecules/FeedSearch.tsx
+++ weatherfit_refactoring/src/Components/Molecules/FeedSearch.tsx
@@ -8,36 +8,35 @@ import { ChangeEventHandler, useEffect, useState } from "react";
 export default function FeedSearch(){
 
     const [hashValue, setHashValue] = useState<string>("");
+    let url = "https://www.jerneithe.site/board/search?categories="
 
-    const searchCancle = () => {
+    const inputHashTag = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setHashValue(e.target.value)
+    }
+
+    const hashTagArrayClear = () => {
         setHashValue("");
     }
 
-    const searchHashTag:ChangeEventHandler<HTMLInputElement> = (e:React.ChangeEvent<HTMLInputElement>) => {
-        console.log("검색할 내용", e.target.value);
-        setHashValue(e.target.value);
-    }
-
-    useEffect(()=>{
+    const searchHashTag = async() => {
         
-        console.log("검색할려고 하는 태그", hashValue);
-        hashToArray();
+        hashToArray()
+        //백엔드에서 따로 검색 구현할 시
+        // const callHashData = await fetch(url, {
+        //     method: "GET"
+        // })
 
-    },[hashValue]);
+        // const callDataToJson = await callHashData.json()
+    }
 
     const hashToArray = () => {
         const searchHashtagData:string[] = hashValue.split('#').filter(Boolean);
         console.log('해시태그 검색 배열', searchHashtagData);
         
-        let url = "https://www.jerneithe.site/board/search?categories="
-        
-        if(searchHashtagData.length != 0) {
-            for(let i=0 ; i<searchHashtagData.length ; i++) {
-                url += searchHashtagData[i];
-            }
+        for(let i=0; i<searchHashtagData.length ; i++) { 
+                url +=searchHashtagData[i];
         }
-
-        console.log("검색 url", url);
+        console.log("검색 url", url)
     }
 
 
@@ -46,21 +45,24 @@ export default function FeedSearch(){
         <div className=" flex py-[10px] px-[10px]">
             <div className="flex border rounded-[9px] mx-1">
                 <InputStore 
+                    value={hashValue}
+                    onChageFunction={inputHashTag}
                     inputStyle={InputStyle.INPUT_SEARCH} 
+                    inputType="text"
                     placeholderContents="#해시태그를 입력하세요" 
                     style="font-gmarketsans font-thin outline-none text-[14px] w-[65vw] h-[38px] mx-1"
-                    value={hashValue}
-                    onChageFunction={searchHashTag}
                     />
                 <IconStore 
                     iconStyle={IconStyle.SEARCH} 
                     size={16} 
-                    style="mr-[10px]"/>
+                    style="mr-[10px]"
+                    onClickFunction={searchHashTag}
+                    />
             </div>
             <ButtonStore 
                 buttonStyle={ButtonStyle.CANCEL_BTN} 
                 style="font-NanumSquareRound text-gray-500 text-[16px] w-[70px]" 
-                onClickFunction={searchCancle}>
+                onClickFunction={hashTagArrayClear}>
                 취소
             </ButtonStore>
         </div>

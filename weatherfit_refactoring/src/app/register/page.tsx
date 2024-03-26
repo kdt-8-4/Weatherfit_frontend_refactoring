93b43137476c6bc2fb609a50a97a6bbd84de9428
nebulaBdj <dongdung5070@gmail.com>
2024-03-26 19:10:13 +0900

    [Feat]Register

--- weatherfit_refactoring/src/app/register/page.tsx
+++ weatherfit_refactoring/src/app/register/page.tsx
@@ -1,20 +1,17 @@
 import InputStore from "@/Components/Atoms/Input/InputStore"
 import RegisterLogo from "@/Components/Molecules/RegisterLogo"
-import RegisterEmail from "@/Components/Molecules/RegisterEmail"
-import RegisterInput from "@/Components/Molecules/RegisterInput"
-import RegisterDone from "@/Components/Molecules/RegisterDone"
-
+import RegisterForm from "@/Components/Organisms/RegisterForm"
+import NavBar from "@/Components/Molecules/NavBar"
 
 export default function Register() {
 
 
 
     return (
-        <div className="flex flex-col items-center mt-[75px]">
+        <div className="flex flex-col items-center mt-[30px]">
             <RegisterLogo />
-            <RegisterEmail />
-            <RegisterInput />
-            <RegisterDone />
+            <RegisterForm />
+            <NavBar/>
         </div>
     )
 }

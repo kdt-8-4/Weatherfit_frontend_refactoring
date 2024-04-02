bbcfd5d01b4a8017778aaa6a5ab763fe3804ce13
hyeiis <hyeis0807@gmail.com>
2024-04-02 09:34:34 +0900

    [Fix] #3 코드 수정

--- weatherfit_refactoring/src/app/feed/detail/[id]/edit/page.tsx
+++ weatherfit_refactoring/src/app/feed/detail/[id]/edit/page.tsx
@@ -8,8 +8,8 @@ export default function Edit({ params }: { params: { id: string } }) {
   const { id: boardId } = params
 
   return (
-    <div className="h-screen ">
+    <>
       <EditOrganism boardId={boardId} />
-    </div>
+    </>
   )
 }
